840fa074936e3830fca9b5000c3c719b0d3b58d9
jitho <jithovjoy@gmail.com>
2021-05-10 12:24:18 +0530

    deletion operation in Doubly Linked List

--- ds/LinkedList/DoublyLinkedList.java
+++ ds/LinkedList/DoublyLinkedList.java
@@ -8,6 +8,22 @@ class Node{
 
 public class DoublyLinkedList {
 	Node head;
+
+	int lengthOfList(){
+	
+		Node tmp = head;
+		int count =0;
+		if(tmp == null){
+			return count;
+		}
+		while(tmp != null){
+			count++;
+			tmp = tmp.next;
+		}
+
+		return count;
+	}
+
 	
 	void insert(int x) {
 		Node node = new Node();
@@ -29,6 +45,70 @@ public class DoublyLinkedList {
 			
 			System.out.println( x+" Succesfully inserted\n");
 		}
+	}
+
+	void insertAtLocation(int loc , int x){
+		int temp = loc;
+		Node node = new Node();
+		node.data = x;
+		node.prev = null;
+		node.next = null;
+
+		if(loc == 1 && head == null){ // empty list
+			head = node;
+			System.out.println( x+" Succesfully inserted at "+ temp+"\n");
+			return;
+		}
+
+		if(loc == 1){
+			node.next = head.next;
+			head.prev = node;
+			head = node;
+			System.out.println( x+" Succesfully inserted at "+temp+"\n");
+			return;
+		}
+
+		if(loc > lengthOfList()){
+			System.out.println("location exceeded the length of list\n");
+			return;
+		}
+
+		Node tmp = head;
+		while(loc-2 > 0){
+			tmp = tmp.next;
+			loc--;
+		}
+		node.next = tmp.next;
+		node.next.prev = node;
+		tmp.next = node;
+		node.prev = tmp;
+
+		System.out.println( x+" Succesfully inserted at "+temp+"\n");
+
+
+	}
+
+	void deleteAt(int loc){
+		int temp = loc;
+		if(loc > lengthOfList()){
+			System.out.println("location exceeded the length of the linked list\n");
+			return;
+		}
+		
+		Node tmp = head;
+		Node pre = head;
+		while(loc-1 >0 ){
+			pre = tmp;
+			tmp = tmp.next;
+			loc--;
+		}
+		pre.next = tmp.next;
+		tmp.next.prev = pre;
+
+		System.out.println("Successfully deleted the element at "+ temp+"\n");
+
+
+
 	}
 	
 	void display() {
@@ -69,6 +149,26 @@ public class DoublyLinkedList {
 
                 doubly.display();
                 doubly.displayReverse();
+
+
+		doubly.insertAtLocation(2, 6);
+		doubly.insertAtLocation(3, 7);
+
+		doubly.display();
+		doubly.displayReverse();
+
+		doubly.deleteAt(2);
+		doubly.display();
+                doubly.displayReverse();
+
+
+		doubly.deleteAt(4);
+
+		doubly.display();
+		doubly.displayReverse();
+
+
+		
 
         }
 
