3a814ccd3a098482b03486f636baa198fc33ed86
jitho <jithovjoy@gmail.com>
2021-05-18 23:21:17 +0530

    java strings basics

---
+++ Strings/Main.java
@@ -0,0 +1,13 @@
+class Main{
+	public static void main(String args[]){
+		String str = "Hello world";// string literal
+		char name[] = {'h','e', 'l', 'l', 'o'};
+		String str2 = new String(name);
+
+		String str3 = new String("Zoho");
+
+		System.out.println(str);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
