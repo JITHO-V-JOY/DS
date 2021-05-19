31e5f59e84f95c42541b2d1f6e7e99a44d9deb62
jitho <jithovjoy@gmail.com>
2021-05-19 22:12:14 +0530

    substring and compareTo

--- Strings/Main.java
+++ Strings/Main.java
@@ -9,5 +9,18 @@ class Main{
 		System.out.println(str);
 		System.out.println(str2);
 		System.out.println(str3);
+
+		// substring
+		System.out.println("an substring of "+str+" "+str.substring(1, 4));
+ 		System.out.println("an substring of "+str3+" "+str3.substring(0, 2));
+		
+		// lexicographical ordering
+		
+		if(str3.compareTo(str2) < 1){
+			System.out.println(str3+" is greater than "+str);
+		}else{
+			 System.out.println(str3+" is less than "+str);
+		}
 	}
+
 }
